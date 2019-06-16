d712b43028738a4e921437a6d151a5ea5f93cff8
Dan <dan@fairedev.com>
2019-06-16 00:31:52 -0400

    improved logging

--- gptimelapse.py
+++ gptimelapse.py
@@ -2,21 +2,23 @@ from gopropyapi.goprocam import GoProCamera
 from gopropyapi.goprocam import constants
 import time, datetime
 from threading import Timer
-import sys
+import sys, os
 
 
 class gpTimeLapse:
-    gpCam = GoProCamera.GoPro()
+    # gpCam = GoProCamera.GoPro()
     interval = 10  # seconds between shots
 
     def __init__(self):
         # redirect stdout to file
         # ref: https://stackoverflow.com/questions/11124093/redirect-python-print-output-to-logger/11124247
-        log = open("timelapse_" + time.time() + ".log", "a")
+        path = os.path.join(os.path.dirname(__file__), "log/timelapse_" + str(time.time()) + ".log")
+        print("Now going to redirect log output to file at ", path, "- Bye bye!")
+        log = open(path, "a")
         sys.stdout = log
 
     def take_photo(self):
-        path = self.gpCam.take_photo(0)
+        path = "test" # self.gpCam.take_photo(0)
         return path
 
     def run_timelapse(self):
@@ -27,10 +29,11 @@ class gpTimeLapse:
 
         print(formatted_time, " captured photo and saved to ", path)
         Timer(self.interval, self.run_timelapse).start()
+        print("start timer for ", self.interval)
 
 
 if __name__ == '__main__':
     print("Running timelapse...")
     gpTimelapse = gpTimeLapse()
-    gpTimelapse.take_photo()
+    gpTimelapse.run_timelapse()
 
